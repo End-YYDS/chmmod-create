46a9e58e74c315ed54f4c0cb9bf3a56dcf71d430
derrick921213 <dlin12457@gmail.com>
2025-01-29 21:09:27 +0800

    fix: 修正Cargo.toml 內的deps

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,27 @@ fn main() {
                 .action(clap::ArgAction::Set)
                 .help("Name of the module"),
         )
+        .arg(
+            Arg::new("description")
+                .short('d')
+                .long("description")
+                .action(clap::ArgAction::Set)
+                .help("Description of the plugin"),
+        )
+        .arg(
+            Arg::new("pversion")
+                .short('v')
+                .long("plugin-version")
+                .action(clap::ArgAction::Set)
+                .help("Plugin version"),
+        )
+        .arg(
+            Arg::new("scope")
+                .short('s')
+                .long("scope")
+                .action(clap::ArgAction::Set)
+                .help("Plugin scope"),
+        )
         .get_matches();
     let module_name = matches
         .get_one::<String>("name")
@@ -52,8 +73,64 @@ fn main() {
                 }
             }
         });
+    let description = matches
+        .get_one::<String>("description")
+        .cloned()
+        .unwrap_or_else(|| {
+            println!("Please enter a plugin description:");
+            let mut input = String::new();
+            match std::io::stdin().read_line(&mut input) {
+                Ok(_) => input.trim().to_string(),
+                Err(_) => {
+                    eprintln!("Failed to read description, exiting...");
+                    std::process::exit(1);
+                }
+            }
+        });
+    let version = matches
+        .get_one::<String>("pversion")
+        .cloned()
+        .unwrap_or_else(|| {
+            println!("Please enter the plugin version (default: 0.1.0):");
+            let mut input = String::new();
+            match std::io::stdin().read_line(&mut input) {
+                Ok(_) => {
+                    let version = input.trim();
+                    if version.is_empty() {
+                        "0.1.0".to_string()
+                    } else {
+                        version.to_string()
+                    }
+                }
+                Err(_) => {
+                    eprintln!("Failed to read version, using default: 0.1.0");
+                    "0.1.0".to_string()
+                }
+            }
+        });
+    let scope = matches
+        .get_one::<String>("scope")
+        .cloned()
+        .unwrap_or_else(|| {
+            println!("Please enter the plugin scope (default: public):");
+            let mut input = String::new();
+            match std::io::stdin().read_line(&mut input) {
+                Ok(_) => {
+                    let scope = input.trim();
+                    if scope.is_empty() {
+                        "public".to_string()
+                    } else {
+                        scope.to_string()
+                    }
+                }
+                Err(_) => {
+                    eprintln!("Failed to read scope, using default: public");
+                    "public".to_string()
+                }
+            }
+        });
 
-    if let Err(e) = scaffold_module(&module_name) {
+    if let Err(e) = scaffold_module(&module_name, &version, &description, &scope) {
         eprintln!(
             "Error: Failed to scaffold the module '{}'. Reason: {}",
             module_name, e
@@ -68,13 +145,20 @@ fn main() {
 /// # Arguments
 ///
 /// * `module_name` - 模組名稱
+/// * `version` - 模組版本
+/// * `description` - 模組描述
+/// * `scope` - 模組作用域
 ///
 /// # Returns
 ///
 /// * `Result<(), Box<dyn std::error::Error>>` - 成功返回 Ok(()), 失敗返回錯誤
-fn scaffold_module(module_name: &str) -> Result<(), Box<dyn std::error::Error>> {
-    create_new_lib(module_name);
-    // create_mod_toml(module_name).map_err(|e| format!("Failed to create mod.toml. {}", e))?;
+fn scaffold_module(
+    module_name: &str,
+    version: &str,
+    description: &str,
+    scope: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    create_new_lib(module_name, version, description, scope)?;
     create_build_workflow(module_name)
         .map_err(|e| format!("Failed to create build workflow. {}", e))?;
     update_cargo_toml(module_name)?;
@@ -86,7 +170,16 @@ fn scaffold_module(module_name: &str) -> Result<(), Box<dyn std::error::Error>> 
 /// # Arguments
 ///
 /// * `module_name` - 模組名稱
-fn create_new_lib(module_name: &str) {
+/// * `version` - 模組版本
+/// * `description` - 模組描述
+/// * `scope` - 模組作用域
+fn create_new_lib(
+    module_name: &str,
+    version: &str,
+    description: &str,
+    scope: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let plugin_name = format!("{}_Plugin", &module_name);
     let status = ProcessCommand::new("cargo")
         .arg("new")
         .arg("--lib")
@@ -98,24 +191,42 @@ fn create_new_lib(module_name: &str) {
         eprintln!("Failed to create new library with cargo");
         std::process::exit(1);
     }
-
     println!("Created new library '{}'", module_name);
-}
+    let lib_content = format!(
+        r#"use actix_web::Responder;
+use plugin_lib::{{declare_plugin, register_plugin}};
 
-#[allow(dead_code)]
-fn create_mod_toml(module_name: &str) {
-    let mod_content = format!(
-        r#"[module]
-name = "{}"
-version = "0.1.0"
-description = "This is the {} module"
-"#,
-        module_name, module_name
+#[derive(Debug)]
+struct {plugin_name};
+
+impl {plugin_name} {{
+    pub fn new() -> Self {{
+        Self
+    }}
+
+    async fn test() -> impl Responder {{
+        "{description}"
+    }}
+}}
+
+declare_plugin!(
+    {plugin_name},
+    meta: {{"{plugin_name}","{version}", "{description}","/{scope}",""}},
+    "",
+    functions:{{
+        "/test" => {{
+            method: actix_web::web::get(),
+            handler: {plugin_name}::test
+        }}
+    }}
+);
+
+register_plugin!({plugin_name});"#
     );
-
-    let mod_toml_path = format!("{}/Mod.toml", module_name);
-    fs::write(&mod_toml_path, mod_content).expect("Failed to create Mod.toml");
-    println!("Created Mod.toml for module '{}'", module_name);
+    let lib_path = format!("{}/src/lib.rs", module_name);
+    fs::write(&lib_path, lib_content)?;
+    println!("Updated lib.rs content for '{}'", module_name);
+    Ok(())
 }
 
 /// 更新 Cargo.toml 配置
@@ -146,10 +257,10 @@ fn update_cargo_toml(module_name: &str) -> Result<(), Box<dyn std::error::Error>
             // table["features"] = value(vec!["plugin_macro"]);
             table
         });
-        dependencies
-            .as_table_mut()
-            .unwrap()
-            .insert("plugin_lib", plugin_lib);
+        let actix_web_lib = value("4.9.0");
+        let dependencies_table = dependencies.as_table_mut().unwrap();
+        dependencies_table.insert("plugin_lib", plugin_lib);
+        dependencies_table.insert("actix-web", actix_web_lib);
     }
 
     let lib_section = doc
